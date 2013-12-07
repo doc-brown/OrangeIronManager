118d2f423a46217a8ea09f659ab88d691001eb37
Robert Mathes <robert.d.mathes@gmail.com>
2013-12-07 13:24:07 +0100

    Added new lesson form

--- js/orangeiron.js
+++ js/orangeiron.js
@@ -3,10 +3,35 @@ function OrangeIronCtrl($scope, $http) {
 	$scope.server = {};
 	$scope.lessons = [];
 
+	$scope.newAlternativeTranslations = [];
+	$scope.newVocabulary = [];
+	$scope.newWord = {};
+	$scope.newWords = [];
+
 	$scope.getData = function() {
 		$http.get('https://googledrive.com/host/0B5pL2OLIkCeiN00xdnVyRGszTmM/albert2.json').success(function(data) {
 	    $scope.server = data;
 	    $scope.lessons = data.lessons;
 	  });
+	}
+
+	$scope.addLesson = function() {
+		$scope.server.lessons.push({name:$scope.newLessonName, version:1, vocabulary:$scope.newVocabulary});
+		$scope.newLessonName = '';
+	}
+
+	$scope.addTranslation = function() {
+		$scope.newAlternativeTranslations.push($scope.newAlternativeTranslation);
+		$scope.newAlternativeTranslation = '';
+	}
+
+	$scope.addWord = function() {
+		$scope.newWord = {originalWord:$scope.newOriginalWord, correctTranslation:$scope.newCorrectTranslation, alternativeTranslations:$scope.newAlternativeTranslations};
+		$scope.newVocabulary.push($scope.newWord);
+		$scope.newWords.push($scope.newWord);
+		$scope.newOriginalWord = '';
+		$scope.newCorrectTranslation = '';
+		$scope.newAlternativeTranslations = [];
+		$scope.newAlternativeTranslation = '';
 	}
 }
